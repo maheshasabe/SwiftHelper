b233804d4bbbea4fd942a0bf2b22a544f6e4b6a5
Mahesh Asabe <mahesh.asabe@in-it0427.globant.com>
2018-10-16 14:20:43 +0530

    Added example file

---
+++ SwiftHelperExample/Pods/Target Support Files/Pods-SwiftHelperExampleTests/Pods-SwiftHelperExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftHelperExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftHelperExampleTests
+@end
---
+++ SwiftHelperExample/Pods/Target Support Files/SwiftHelper/SwiftHelper-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftHelper : NSObject
+@end
+@implementation PodsDummy_SwiftHelper
+@end
