b6501ebd0897bd33c0a12cbf48ffa606b9855571
Mahesh Asabe <mahesh.asabe@in-it0427.globant.com>
2018-10-16 13:04:42 +0530

    SwiftHelper project created

---
+++ SwiftHelper/SwiftHelper/SwiftHelper.h
@@ -0,0 +1,19 @@
+//
+//  SwiftHelper.h
+//  SwiftHelper
+//
+//  Created by Mahesh Asabe on 16/10/18.
+//  Copyright © 2018 Mahesh Asabe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftHelper.
+FOUNDATION_EXPORT double SwiftHelperVersionNumber;
+
+//! Project version string for SwiftHelper.
+FOUNDATION_EXPORT const unsigned char SwiftHelperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftHelper/PublicHeader.h>
+
+
---
+++ SwiftHelper/SwiftHelper/TestClass.swift
@@ -0,0 +1,16 @@
+//
+//  TestClass.swift
+//  SwiftHelper
+//
+//  Created by Mahesh Asabe on 16/10/18.
+//  Copyright © 2018 Mahesh Asabe. All rights reserved.
+//
+
+import UIKit
+
+class TestClass: NSObject {
+    
+    class func whatsMyName() -> String {
+        return "Swift Helper TestClass"
+    }
+}
